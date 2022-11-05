7657ef025388c850a19a24827c13a2444925164b
Brian David Garcia Manzanarez <bgarcia@telepro.com.mx>
2022-11-05 10:09:35 -0600

    backend

---
+++ SmallAPI/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using examen_backend_DAVID_GARCIA.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace examen_backend_DAVID_GARCIA.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController: ControllerBase
+    {
+        IUserService userServiceParameter;
+        public UserController(IUserService userServiceParameter)
+        {
+            this.userServiceParameter = userServiceParameter;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(userServiceParameter.Get());
+        }
+    }
+}
